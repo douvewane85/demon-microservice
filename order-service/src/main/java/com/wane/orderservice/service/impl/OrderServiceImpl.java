283421339949143f64eeca8e9934737cfd0fef14
douvewane <douvewane85@gmail.com>
2023-11-05 09:32:06 +0000

    Communication synchrone with WebClient

--- order-service/src/main/java/com/wane/orderservice/service/impl/OrderServiceImpl.java
+++ order-service/src/main/java/com/wane/orderservice/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package com.wane.orderservice.service.impl;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wane.orderservice.data.dto.InventoryResponse;
 import com.wane.orderservice.data.dto.OrderLineItemsDto;
 import com.wane.orderservice.data.dto.OrderRequest;
 import com.wane.orderservice.data.entity.Order;
@@ -11,7 +12,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -20,6 +23,7 @@ import java.util.UUID;
 @Transactional
 public class OrderServiceImpl implements OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClientBuilder;
     @Autowired
     private ObjectMapper objectMapper;
 
@@ -32,6 +36,28 @@ public class OrderServiceImpl implements OrderService {
                 .map(orderLineItemsDto -> objectMapper.convertValue(orderLineItemsDto, OrderLineItems.class))
                 .toList();
           order.setOrderLineItemsList(orderLineItems);
-          orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItemsList().stream()
+                .map(OrderLineItems::getSkuCode)
+                .toList();
+
+        // Call Inventory Service, and place order if product is in
+        // stock
+
+        InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+                .uri("http://localhost:9003/api/v1/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCodes", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponsArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if(allProductsInStock){
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 }
